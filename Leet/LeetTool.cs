9638249de609cf3ae645f35246be363f9c0efa17
OlisGamingKeller <227825798+OlisGamingKeller@users.noreply.github.com>
2026-02-12 21:25:48 +0100

    LeetTool begonnen

--- Leet/LeetTool.cs
+++ Leet/LeetTool.cs
@@ -2,10 +2,18 @@ using System;
 
 namespace ToolboxApp.Leet;
 
+/* Idee:
+LeetTool ist ein Toolbox-Tool mit eigenem Untermenü
+Es dient als Übersetzer von LEET-Speech in beide Richtungen (Klartext <-> LEET)
+Es soll automatisch erkennen, welche Form vorliegt. Man sollte das Ergebnis in einer .txt speichern können
+und auch aus einer solchen einlesen können.
+*/
 public class LeetTool : ITool
 {
     public string Name => "LEET-Speech-Übersetzer (WiP)";
 
+    private readonly Dictionary<char,char> _plainToLeet;
+    private readonly Dictionary<char,char> _leetToPlain;
     public void Run()
     {
         Console.WriteLine("Achtung Baustelle");
--- obj/Debug/net10.0/ToolboxApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ToolboxApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolboxApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e25b52c6cec915cdc7c3efcad5ae714a3e66f12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dbe5882f813196c0f0e5532ea4404993329a7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolboxApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolboxApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
